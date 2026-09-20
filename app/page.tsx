3793379bc929fd91d1cc11c4181979f1bfa37d28
Dev-Gony <gwonyi190000@gmail.com>
2026-09-20 18:30:36 +0900

    feat: expose Reviewus in platform filter

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 export const dynamic = "force-dynamic";
 
 const PAGE_SIZE = 40;
-const PLATFORMS = ["강남맛집", "리뷰노트", "디너의여왕", "미블", "리뷰플레이스", "레뷰"];
+const PLATFORMS = ["강남맛집", "리뷰노트", "디너의여왕", "미블", "리뷰플레이스", "리뷰어스", "레뷰"];
 const MEDIA_TYPES = ["블로그", "인스타그램", "유튜브", "숏폼", "숏폼(릴스)", "블로그+숏폼"];
 const CAMPAIGN_TYPES = ["방문형", "배송형", "포장", "페이백"];
 const REGION_GROUPS = [
